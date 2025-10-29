33a9a2e133c889892f2187b527ae7dfaa3164581
TuNombreDeGitHub <tuemail@ejemplo.com>
2025-10-29 12:07:36 +0100

    445

--- exercises/044-static_methods/app.py
+++ exercises/044-static_methods/app.py
@@ -1,1 +1,8 @@
 # Your code here
+class MathOperations():
+    @staticmethod
+    def add_numbers(a,b):
+        return a+b
+
+operacion1=MathOperations.add_numbers(10,15) #instancia de la clase
+print(operacion1)
--- exercises/045-class_methods/app.py
+++ exercises/045-class_methods/app.py
@@ -1,2 +1,10 @@
 # Your code here
 
+class MathOperations():
+    pi=3.1416
+    @classmethod
+    def calculate_circle_area(cls, radius):
+        return cls.pi * radius * radius
+    
+area = MathOperations.calculate_circle_area(5)
+print(area)  # Output: 78.54
